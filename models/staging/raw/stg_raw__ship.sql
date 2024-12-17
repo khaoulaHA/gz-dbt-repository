f45583a9a8d4c231327b26d3989e62f7459aa484
khawla.habboubi@gmail.com <khawla.habboubi@gmail.com>
2024-12-17 11:54:00 +0000

    staging

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        --shipping_fee_1,
+        logcost,
+        cast(ship_cost as FLOAT64) as ship_cost
+
+    from source
+    where shipping_fee <> shipping_fee_1
+
+)
+
+select * from renamed
