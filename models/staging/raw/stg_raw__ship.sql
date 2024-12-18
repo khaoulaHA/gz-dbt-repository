17d9a63e66bacbbbd8a17565cbb287ffc647861b
khawla.habboubi@gmail.com <khawla.habboubi@gmail.com>
2024-12-18 13:10:53 +0000

    challenge 1

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,11 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        --shipping_fee_1,
         logcost,
         cast(ship_cost as FLOAT64) as ship_cost
 
     from source
-  --  where shipping_fee = shipping_fee_1
+  
 
 )
 
